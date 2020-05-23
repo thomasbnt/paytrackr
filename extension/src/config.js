370e82250a0195f332975e2c4c8dfba710502ba1
Robert Anthonie Soriano <sorianorobertc@gmail.com>
2020-05-23 14:31:30 +0800

    [refactor] Update extension api url

--- extension/src/config.js
+++ extension/src/config.js
@@ -2,7 +2,7 @@ const config = {
   apiUrl:
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3030'
-      : 'http://localhost:3030'
+      : 'https://paytrackr-api.now.sh'
 };
 
 export default config;
